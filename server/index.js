4602695fb517eb1616879a4e2cf6961093e518b1
Shivin-Goel <shivingoel15@gmail.com>
2026-03-12 16:17:28 +0530

    Change CORS origin to production URL

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import interviewRouter from './routes/interviewroute.js';
 
 const app = express();
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://ai-interview-assistant-cyau.onrender.com",
     credentials: true
 }))
 
